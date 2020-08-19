3f6175ea2fbd474055a19a2b98195080ba5af2af
kd993595 <59321014+kd993595@users.noreply.github.com>
2020-08-19 15:46:27 -0400

    Update draw.js

--- LinkedPages/draw.js
+++ LinkedPages/draw.js
@@ -3,7 +3,7 @@ const ctx=canvas.getContext("2d");
 const scale = 10;
 const rows = canvas.height /scale;
 const columns = canvas.width /scale;
-
+console.log(canvas);
 var snake;
 
 /*(function setup{

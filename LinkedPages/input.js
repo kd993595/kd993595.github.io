c83a999ae22c9dbfd31dc0c7be6f9ba26b685f06
kd993595 <59321014+kd993595@users.noreply.github.com>
2020-08-30 22:36:53 -0400

    Update input.js

--- LinkedPages/input.js
+++ LinkedPages/input.js
@@ -24,4 +24,43 @@ window.addEventListener('keydown',e=>{
 export function getInputDirection(){
 	lastInputDirection = inputDirection
 	return inputDirection
+};
+window.addEventListener('touchstart',handleTouchStart,false);
+window.addEventListener('touchmove',handleTouchMove,false);
+var xDown=null;
+var yDown =null;
+
+function getTouches(evt){
+	return evt.touches || evt.originalEvent.touches;
 }
+
+function handleTouchStart(evt){
+	const firstTouch = getTouches(evt[0]);
+	xDown = firstTouch.clientX;
+	yDown = firstTouch.clientY;
+};
+function handleTouchMove(evt){
+	if(!xDown||!yDown){
+		return
+	}
+	var xUp = evt.touches[o].clientX;
+	var yUp = evt.touches[0].clientY;
+	var xDiff = xDown - xUp;
+	var yDiff = yDown-yUp;
+
+	if(Math.abs(xDiff)>Math.abs(yDiff)){
+		if(xDiff>0){
+			//left swipe
+		}else{
+			//right swipe
+		}
+	}else{
+		if(yDiff>0){
+			//upswipe
+		}else{
+			//down swipe
+		}
+	}
+	xDown = null;
+	yDown = null;
+};

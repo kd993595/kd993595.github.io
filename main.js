06cb473261cab5d7499d1ce8aaf3a07f174e3ba2
kd993595 <59321014+kd993595@users.noreply.github.com>
2025-01-08 16:15:11 -0500

    update website new structure

---
+++ main.js
@@ -0,0 +1,54 @@
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    let coll = document.getElementsByClassName("collapsible");
+    let i;
+
+    coll[0].classList.toggle("active");
+    let content = coll[0].nextElementSibling;
+    content.style.maxHeight = content.scrollHeight + "px";
+
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            let content = this.nextElementSibling;
+            if (content.style.maxHeight) {
+                content.style.maxHeight = null;
+            } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+            }
+        });
+    }
+});
+
+function calculateSettingAsThemeString({ localStorageTheme, systemSettingDark }) {
+    if (localStorageTheme !== null) {
+        return localStorageTheme;
+    }
+
+    if (systemSettingDark.matches) {
+        return "dark";
+    }
+
+    return "light";
+}
+
+const localStorageTheme = localStorage.getItem("theme");
+const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
+
+let currentThemeSetting = calculateSettingAsThemeString({ localStorageTheme, systemSettingDark });
+
+document.querySelector("html").setAttribute("data-theme", currentThemeSetting);
+
+function toggleMode() {
+    const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
+
+    document.querySelector("html").setAttribute("data-theme", newTheme);
+
+    // update in local storage
+    localStorage.setItem("theme", newTheme);
+
+    // update the currentThemeSetting in memory
+    currentThemeSetting = newTheme;
+}
+

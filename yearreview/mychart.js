becde18eacb1f6a238fbbc80d9ae317088a29e41
Kevin Durand <kepedu17@gmail.com>
2026-01-24 17:45:56 -0500

    add year review portion

---
+++ yearreview/mychart.js
@@ -0,0 +1,376 @@
+January = [90,75,48,42,84,72,83,76,93,90,70,63,71,87,73,80,72,42,73,92,94,74,82,24,84,42,84,85,76,62,67];
+February = [50,66,74,83,64,93,76,73,90,92,84,64,70,92,70,62,79,83,61,77,77,93,98,97,84,70,75,55];
+March = [68,70,47,96,76,87,60,70,80,90,82,33,88,65,73,23,74,80,72,83,83,11,53,86,84,73,72,86,73,54,80];
+April = [89.3,70,93,80,72,33,76,50,70,93,76,50,20,64,83,72,92,78,80,93,70,80,78,82,79,83,66,84,79,11];
+May = [90,79,80,80,88,92,99,90,76,83,92,96,72,80,83,50,57,66,87,79,80,60,92,80,80,77,56,75,58,80,40];
+June = [36,90,63,60,45,90,75,70,83,84,79,43,94,81,43,80,84,73,94,67,73,30,19,72,89,54,62,94,46,83];
+July = [56,72,63,12,95,91,90,52,76,81,62,77,79,60,75,93,90,79,71,63,80,50,90,87,79,72,54,90,66,82,88];
+Aug = [68,74,22,92,74,74,78,94,93,92,84,71,79,82,44,22,76,74,86,77,90,20,34,54,75,80,50,80,49,30,12];
+September = [42,52,84,70,74,67,44,80,86,82,90,78,62,70,72,83,60,90,75,55,42,40,94,88,75,80,60,60,86,54];
+October = [78,90,40,55,96,60,60,85,72,80,70,65,50,90,80,55,32,54,60,92,74,90,71,81,69,94,91,75,89,90,90];
+November = [70,73,90,64,92,70,76,80,80,86,72,88,91,70,68,73,94,78,90,96,83,75,68,83,64,92,85,53,63,80];
+December = [92,76,88,93,62,42,21,83,64,89,74,69,66,53,79,90,84,90,81,85,66,90,74,82,88,72,80,62,77,64,93];
+FULLYEAR = [90,75,48,42,84,72,83,76,93,90,70,63,71,87,73,80,72,42,73,92,94,74,82,24,84,42,84,85,76,62,67,50,66,74,83,64,93,76,73,90,92,84,64,70,92,70,62,79,83,61,77,77,93,98,97,84,70,75,55,68,70,47,96,76,87,60,70,80,90,82,33,88,65,73,23,74,80,72,83,83,11,53,86,84,73,72,86,73,54,80,89.3,70,93,80,72,33,76,50,70,93,76,50,20,64,83,72,92,78,80,93,70,80,78,82,79,83,66,84,79,11,90,79,80,80,88,92,99,90,76,83,92,96,72,80,83,50,57,66,87,79,80,60,92,80,80,77,56,75,58,80,40,36,90,63,60,45,90,75,70,83,84,79,43,94,81,43,80,84,73,94,67,73,30,19,72,89,54,62,94,46,83,56,72,63,12,95,91,90,52,76,81,62,77,79,60,75,93,90,79,71,63,80,50,90,87,79,72,54,90,66,82,88,68,74,22,92,74,74,78,94,93,92,84,71,79,82,44,22,76,74,86,77,90,20,34,54,75,80,50,80,49,30,12,42,52,84,70,74,67,44,80,86,82,90,78,62,70,72,83,60,90,75,55,42,40,94,88,75,80,60,60,86,54,78,90,40,55,96,60,60,85,72,80,70,65,50,90,80,55,32,54,60,92,74,90,71,81,69,94,91,75,89,90,90,70,73,90,64,92,70,76,80,80,86,72,88,91,70,68,73,94,78,90,96,83,75,68,83,64,92,85,53,63,80,92,76,88,93,62,42,21,83,64,89,74,69,66,53,79,90,84,90,81,85,66,90,74,82,88,72,80,62,77,64,93,93];
+Monday = [72,71,92,84,74,92,79,97,47,90,74,86,80,76,64,70,84,88,96,87,77,90,83,80,19,83,90,60,80,90,92,84,74,75,42,80,72,40,86,60,50,92,91,90,86,94,83,92,83,79,90,77];
+Tuesday = [83,87,94,85,83,84,83,84,96,82,80,84,89.3,50,83,80,79,92,72,79,56,63,84,84,72,56,52,75,50,66,74,71,86,80,52,86,83,94,54,60,90,74,75,64,72,78,64,76,64,90,74,64];
+Wednesday = [90,76,73,74,76,64,64,61,70,76,33,72,73,70,70,72,78,11,99,80,80,75,60,79,73,89,72,76,93,90,82,74,79,77,50,84,82,60,88,78,85,80,90,89,92,88,90,92,88,89,84,82,93];
+Thursday = [75,93,80,82,62,93,70,77,75,87,88,83,72,93,93,92,82,90,90,83,60,58,45,43,94,54,63,81,90,87,88,78,82,90,80,70,90,90,75,90,72,55,71,90,70,91,96,85,93,74,90,88];
+Friday = [48,90,72,24,67,76,92,77,55,60,65,83,86,80,76,78,79,79,76,50,92,80,90,94,67,62,12,62,79,79,68,94,44,20,49,74,78,75,80,40,80,32,81,90,76,70,83,53,62,69,81,72];
+Saturday = [42,70,42,84,50,73,70,93,68,70,73,11,73,72,50,80,83,80,83,57,80,40,75,81,73,94,95,77,71,72,74,93,22,34,30,67,62,55,60,55,70,54,69,70,80,68,75,63,42,66,85,80];
+Sunday = [84,63,73,42,66,90,62,98,70,80,23,53,54,33,20,93,66,80,92,66,80,36,70,43,30,46,91,79,63,54,22,92,76,54,12,44,70,42,60,96,65,60,94,73,80,73,68,80,21,53,66,62];
+Mins = [24, 50, 11, 11, 40, 19, 12, 12, 40, 32, 53, 21];
+Maxs = [94, 98, 96, 93, 99, 94, 95, 94, 94, 96, 96, 93];
+Means = [72.58, 76.86, 70.06, 71.54, 77.32, 68.53, 73.39, 65.48, 69.83, 73.48, 78.23, 75.13];
+Median = [75, 76.5, 73, 78, 80, 73, 77, 74, 73, 75, 79, 79];
+WeekMean = [78.21,75.62,76.7,79.67,69.25,66.46,62.75];
+WeekMedian = [83.0,79.0,78,82.5,76.0,70.0,66.0];
+WeekMin = [19,50,11,43,12,11,12];
+WeekMax = [97,96,99,96,94,95,98];
+
+
+startDate = new Date(2025, 0, 1);
+dayStr = [];
+
+for (let i = 0; i < 365; i++) {
+  dayStr.push(startDate.toDateString());
+  startDate.setDate(startDate.getDate()+1);
+}
+
+
+const ctx1 = document.getElementById('chart1');
+//Animation
+const totalDuration = 10000;
+const delayBetweenPoints = totalDuration / 365;
+const previousY = (ctx1) => ctx1.index === 0 ? ctx1.chart.scales.y.getPixelForValue(100) : ctx1.chart.getDatasetMeta(ctx1.datasetIndex).data[ctx1.index - 1].getProps(['y'], true).y;
+const animation = {
+  x: {
+    type: 'number',
+    easing: 'linear',
+    duration: delayBetweenPoints,
+    from: NaN, // the point is initially skipped
+    delay(ctx1) {
+      if (ctx1.type !== 'data' || ctx1.xStarted) {
+        return 0;
+      }
+      ctx1.xStarted = true;
+      return ctx1.index * delayBetweenPoints;
+    }
+  },
+  y: {
+    type: 'number',
+    easing: 'linear',
+    duration: delayBetweenPoints,
+    from: previousY,
+    delay(ctx1) {
+      if (ctx1.type !== 'data' || ctx1.yStarted) {
+        return 0;
+      }
+      ctx1.yStarted = true;
+      return ctx1.index * delayBetweenPoints;
+    }
+  }
+};
+new Chart(ctx1,{
+  type: 'line',
+  data: {
+    labels: dayStr,
+    datasets: [
+      {
+        label: 'Line Values',
+        data: FULLYEAR,
+        borderColor: 'rgb(234, 162, 57)',
+        backgroundColor: 'rgb(234, 162, 57)',
+        tension: 0.4
+      }
+    ]
+
+  },
+  options: {
+    animation,
+    interaction: {
+      intersect: false
+    },
+    scales: {
+      y:{min:0,max:100},
+    },
+    responsive: true,
+    plugins: {
+      title: {
+        display: true,
+        text: 'Values over the Year'
+      }
+    }
+  }
+});
+
+
+const ctx2 = document.getElementById('chart2');
+new Chart(ctx2, {
+	type: 'bar',
+	data: {
+		labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+		datasets: [{
+				label: 'Mean',
+				data: Means,
+				backgroundColor: 'rgba(54, 162, 235, 0.5)',
+			  borderColor: 'rgb(54, 162, 235)',
+				borderWidth: 2,
+				borderRadius: 5,
+				borderSkipped: false
+			},
+			{
+				label: 'Median',
+				data: Median,
+				backgroundColor: 'rgba(242, 65, 133, 0.5)',
+				borderColor: 'rgb(242, 65, 133)',
+				borderWidth: 2,
+				borderRadius: 5,
+				borderSkipped: false
+			},
+			{
+				label: 'Min',
+				data: Mins,
+				backgroundColor: 'rgb(29, 222, 103, 0.5)',
+				borderColor: 'rgb(29, 222, 103)',
+				borderWidth: 2,
+				borderRadius: 5,
+				borderSkipped: false
+			},
+			{
+				label: 'Max',
+				data: Maxs,
+				backgroundColor: 'rgba(145, 36, 240, 0.5)',
+				borderColor: 'rgb(145, 36, 240)',
+				borderWidth: 2,
+				borderRadius: 5,
+				borderSkipped: false
+			}
+		]
+	},
+	options: {
+		scales: {
+			y: {
+				min: 0,
+				max: 100
+			}
+		},
+		responsive: true,
+		plugins: {
+			legend: {
+				position: 'top',
+			},
+			title: {
+				display: true,
+				text: 'General Statistics'
+			}
+		}
+	}
+});
+
+const ctx3 = document.getElementById("chart3");
+new Chart(ctx3, {
+  type: 'radar',
+  data: {
+    labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+    datasets: [
+      {
+        label: 'Mean',
+        data: WeekMean,
+        borderColor: 'rgb(144, 158, 37)',
+        backgroundColor: 'rgba(144, 158, 37, 0.5)',
+        pointRadius: 6
+      },
+      {
+        label: 'Median',
+        data: WeekMedian,
+        borderColor: 'rgb(195, 165, 193)',
+        backgroundColor: 'rgba(195, 165, 193, 0.5)',
+        pointRadius: 6,
+      },
+      {
+        label: 'Max',
+        data: WeekMax,
+        borderColor: 'rgb(151, 51, 44)',
+        backgroundColor: 'rgba(151, 51, 44, 0.5)',
+        pointRadius: 6,
+      },
+      {
+        label: 'Min',
+        data: WeekMin,
+        borderColor: 'rgb(229, 151, 147)',
+        backgroundColor: 'rgba(229, 151, 147, 0.5)',
+        pointRadius: 6,
+      }
+    ]
+  },
+  options: {
+    scales: {
+      r: {min:0,max:100}
+    },
+    responsive: true,
+    plugins: {
+      title: {
+        display: true,
+        text: 'By Day Radar'
+      }
+    }
+  }
+});
+
+const ctx4 = document.getElementById("chart4");
+const totalDuration2 = 5000;
+const delayBetweenPoints2 = totalDuration2 / 31;
+const previousY2 = (ctx4) => ctx4.index === 0 ? ctx4.chart.scales.y.getPixelForValue(100) : ctx4.chart.getDatasetMeta(ctx4.datasetIndex).data[ctx4.index - 1].getProps(['y'], true).y;
+const animation2 = {
+  x: {
+    type: 'number',
+    easing: 'linear',
+    duration: delayBetweenPoints2,
+    from: NaN, // the point is initially skipped
+    delay(ctx4) {
+      if (ctx4.type !== 'data' || ctx4.xStarted) {
+        return 0;
+      }
+      ctx4.xStarted = true;
+      return ctx4.index * delayBetweenPoints2;
+    }
+  },
+  y: {
+    type: 'number',
+    easing: 'linear',
+    duration: delayBetweenPoints2,
+    from: previousY2,
+    delay(ctx4) {
+      if (ctx4.type !== 'data' || ctx4.yStarted) {
+        return 0;
+      }
+      ctx4.yStarted = true;
+      return ctx4.index * delayBetweenPoints2;
+    }
+  }
+};
+const labelNums = [];
+for (let i = 1; i < 32; ++i) {
+  labelNums.push(i.toString());
+}
+new Chart(ctx4,{
+  type: 'line',
+  data: {
+    labels: labelNums,
+    datasets: [
+      {
+        label: 'January',
+        data: January,
+        borderColor: 'rgb(230, 25, 75)',
+        backgroundColor: 'rgba(230, 25, 75, 0.5)',
+        pointRadius: 6,
+        tension: 0.4
+      },
+      {
+        label: 'February',
+        data: February,
+        borderColor: 'rgb(60, 180, 75)',
+        backgroundColor: 'rgba(60, 180, 75, 0.5)',
+        pointRadius: 6,
+        tension: 0.4
+      },
+      {
+        label: 'March',
+        data: March,
+        borderColor: 'rgb(255, 225, 25)',
+        backgroundColor: 'rgba(255, 225, 25, 0.5)',
+        pointRadius: 6,
+        tension: 0.4
+      },
+      {
+        label: 'April',
+        data: April,
+        borderColor: 'rgb(0, 130, 200)',
+        backgroundColor: 'rgba(0, 130, 200, 0.5)',
+        pointRadius: 6,
+        tension: 0.4
+      },
+      {
+        label: 'May',
+        data: May,
+        borderColor: 'rgb(245, 130, 48)',
+        backgroundColor: 'rgba(245, 130, 48, 0.5)',
+        pointRadius: 6,
+        tension: 0.4
+      },
+      {
+        label: 'June',
+        data: June,
+        borderColor: 'rgb(145, 30, 180)',
+        backgroundColor: 'rgba(145, 30, 180, 0.5)',
+        pointRadius: 6,
+        tension: 0.4
+      },
+      {
+        label: 'July',
+        data: July,
+        borderColor: 'rgb(70, 240, 240)',
+        backgroundColor: 'rgba(70, 240, 240, 0.5)',
+        pointRadius: 6,
+        tension: 0.4
+      },
+      {
+        label: 'August',
+        data: Aug,
+        borderColor: 'rgb(240, 50, 230)',
+        backgroundColor: 'rgba(240, 50, 230, 0.5)',
+        pointRadius: 6,
+        tension: 0.4
+      },
+      {
+        label: 'September',
+        data: September,
+        borderColor: 'rgb(0, 128, 128)',
+        backgroundColor: 'rgba(0, 128, 128, 0.5)',
+        pointRadius: 6,
+        tension: 0.4
+      },
+      {
+        label: 'October',
+        data: October,
+        borderColor: 'rgb(220, 190, 255)',
+        backgroundColor: 'rgba(220, 190, 255, 0.5)',
+        pointRadius: 6,
+        tension: 0.4
+      },
+      {
+        label: 'November',
+        data: November,
+        borderColor: 'rgb(170, 255, 195)',
+        backgroundColor: 'rgba(170, 255, 195, 0.5)',
+        pointRadius: 6,
+        tension: 0.4
+      },
+      {
+        label: 'December',
+        data: December,
+        borderColor: 'rgb(128, 128, 128)',
+        backgroundColor: 'rgba(128, 128, 128,0.5)',
+        pointRadius: 6,
+        tension: 0.4
+      }
+    ]
+
+  },
+  options: {
+    animation2,
+    interaction: {
+      intersect: false
+    },
+    scales: {
+      y:{min:0,max:100},
+    },
+    responsive: true,
+    plugins: {
+      title: {
+        display: true,
+        text: 'Values over each Month'
+      }
+    }
+  }
+});
